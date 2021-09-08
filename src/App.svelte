e953b10ac15117dabc5c6cb4138568c9ce953df7
DASEUL KIM <daseulkim@DASEULui-MacBookPro.local>
2021-09-09 00:55:23 +0900

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,30 @@
+<script>
+		
+	import Myview from './Myview.svelte';
+	export let name;
+
+</script>
+
+<style>
+	main {
+		text-align: center;
+		padding:1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 40px;
+		font-weight:100;
+	}
+	@media (min-width: 640px){
+		main {
+			max-width: none;
+		} 
+	}
+</style>
+<main>
+	<h1>Hello {name}!</h1>
+	<Myview/>
+</main>
---
+++ src/MyView.svelte
@@ -0,0 +1,3 @@
+<main>
+    저는 myview입니당
+</main>
